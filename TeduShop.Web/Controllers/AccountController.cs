85bda6c1d22647e184ce4b3841740b440e6dc629
ST-PC\ST <vanhongit@gmail.com>
2018-10-24 15:37:08 +0700

    register page

--- TeduShop.Web/Controllers/AccountController.cs
+++ TeduShop.Web/Controllers/AccountController.cs
@@ -1,14 +1,56 @@
-﻿using System;
+﻿using BotDetect.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TeduShop.Common;
+using TeduShop.Model.Models;
+using TeduShop.Web.App_Start;
 using TeduShop.Web.Models;
 
 namespace TeduShop.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController()
+        {
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         // GET: Account
         public ActionResult Login()
         {
@@ -22,8 +64,58 @@ namespace TeduShop.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult Register(RegisterViewModel registerViewModel)
+        [CaptchaValidation("CaptchaCode", "registerCaptcha", "Mã xác nhận không đúng!")]
+        public async Task<ActionResult> Register(RegisterViewModel registerViewModel)
         {
+            if (ModelState.IsValid)
+            {
+                var userEmail =await _userManager.FindByEmailAsync(registerViewModel.Email);
+
+                if (userEmail != null)
+                {
+                    ModelState.AddModelError("", "Email đã tồn tại.");
+                    return View(registerViewModel);
+                }
+
+                var userName =await _userManager.FindByNameAsync(registerViewModel.UserName);
+
+                if (userName != null)
+                {
+                    ModelState.AddModelError("", "Username đã tồn tại.");
+                    return View(registerViewModel);
+                }
+
+                var user = new ApplicationUser()
+                {
+                    UserName = registerViewModel.UserName,
+                    Email = registerViewModel.Email,
+                    EmailConfirmed = true,
+                    BirthDay = DateTime.Now,
+                    FullName = registerViewModel.FullName,
+                    PhoneNumber = registerViewModel.PhoneNumber,
+                    Address = registerViewModel.Address
+
+                };
+
+                await _userManager.CreateAsync(user, registerViewModel.Password);
+
+                var adminUser = await _userManager.FindByEmailAsync(registerViewModel.Email);
+
+                if (adminUser != null)
+                {
+                    await _userManager.AddToRolesAsync(adminUser.Id, new string[] {"User" });
+                }
+
+                string content = System.IO.File.ReadAllText(Server.MapPath("/Assets/client/template/newuser.html"));
+                content = content.Replace("{{UserName}}", adminUser.UserName);
+                content = content.Replace("{{Link}}", ConfigHelper.GetByKey("CurrentLink")+"dang-nhap.html");
+                MailHelper.SendMail(adminUser.Email, "Đăng ký thành công", content);
+
+                ViewData["SuccessMsg"] = "đăng ký thành công.";
+
+            }
+
+
             return View();
         }
     }
