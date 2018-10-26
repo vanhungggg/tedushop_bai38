7e652842552109b28ccf742ff7d947b09b4fb27c
ST-PC\ST <vanhongit@gmail.com>
2018-10-26 13:31:28 +0700

    shopping cart 2

--- TeduShop.Web/Assets/client/js/Controller/shoppingCart.js
+++ TeduShop.Web/Assets/client/js/Controller/shoppingCart.js
@@ -1,22 +1,51 @@
 ﻿var cart = {
     init: function () {
-        
         cart.loadData();
         cart.registerEvent();
     },
     registerEvent: function () {
-        $('#btnAddToCart').off('click').on('click', function (e) {
+        //(cart.getTotalOrder() == null ? '0' : $('#lblTotalOrder').text(cart.getTotalOrder()));
+
+        $('.btnAddToCart').off('click').on('click', function (e) {
             e.preventDefault();
-            
             var productId = parseInt($(this).data('id'));
-            alert(productId);
             cart.addItem(productId);
         });
         $('.btnDeleteItem').off('click').on('click', function () {
 
             var productId = parseInt($(this).data('id'));
+            //var quantity = parseInt($(this).data('quantity'));
+            //var price = parseFloat($(this).data('price'));
+
+            //$('#lblTotalOrder').text(cart.getTotalOrder() - price * quantity);
             cart.deleteItem(productId);
+
         });
+        $('.txtQuantity').off('keyup').on('keyup', function () {//keypress: bam'+enter, keydown: bam' xuong', keyup:bam'
+            var productId = parseInt($(this).data('id'));
+            var quantity = parseInt($(this).val());
+            var price = parseFloat($(this).data('price'));
+            if (isNaN(quantity) == false) { //phai la so'
+                var amount = quantity * price;
+                $('#amount_' + productId).text(numeral(amount).format('0,0'));
+            }
+            else {
+                $('#amount_' + productId).text(0);
+            }
+            $('#lblTotalOrder').text(numeral(cart.getTotalOrder()).format('0,0'));
+        });
+
+        
+    },
+
+    getTotalOrder: function () {
+        var listTextBox = $('.txtQuantity');
+        var total = 0;
+        $.each(listTextBox, function (i, item) {
+            total += parseInt($(item).val()) * parseFloat($(item).data('price'));
+
+        });
+        return total;
     },
 
     addItem: function (id) {
@@ -65,13 +94,14 @@
                             ProductName: item.Product.Name,
                             Image: item.Product.Image,
                             Price: item.Product.Price,
+                            PriceF: numeral(item.Product.Price).format('0,0'),
                             Quantity: item.Quantity,
-                            Amount: item.Quantity * item.Product.Price
+                            Amount: numeral(item.Quantity * item.Product.Price).format('0,0')
                         });
-                        
+
                     });
                     $('#cartBody').html(html);
-
+                    $('#lblTotalOrder').text(numeral(cart.getTotalOrder()).format('0,0'));
                     cart.registerEvent();
                 }
             }
