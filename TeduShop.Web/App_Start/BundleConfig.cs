46d1ab3a3c573df044263346d0053ed42978bee6
ST-PC\ST <vanhongit@gmail.com>
2018-11-05 16:47:17 +0700

    @Scripts.render(), @Styles.render()

--- TeduShop.Web/App_Start/BundleConfig.cs
+++ TeduShop.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,24 @@ namespace TeduShop.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-
+            bundles.Add(new ScriptBundle("~/js/jquery").Include("~/Assets/client/js/jquery.min.js"));
+            bundles.Add(new ScriptBundle("~/js/plugins").Include(
+                "~/Assets/admin/libs/jquery-ui/jquery-ui.min.js",
+                "~/Assets/client/js/common.js",
+                "~/Assets/admin/libs/mustache/mustache.js",
+                "~/Assets/admin/libs/numeral/numeral.js",
+                "~/Assets/admin/libs/jquery-validation/dist/jquery.validate.js",
+                "~/Assets/admin/libs/jquery-validation/dist/additional-methods.js",
+                "~/Assets/client/js/Controller/shoppingCart.js"
+                ));
+            bundles.Add(new StyleBundle("~/css/base")
+                .Include("~/Assets/client/css/bootstrap.css",new CssRewriteUrlTransform())
+                .Include("~/Assets/client/font-awesome-4.7.0/css/font-awesome.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/admin/libs/jquery-ui/themes/smoothness/jquery-ui.min.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/style.css", new CssRewriteUrlTransform())
+                .Include("~/Assets/client/css/custom.css", new CssRewriteUrlTransform())
+                );
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
